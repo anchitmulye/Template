1678c7b47aa204b4c2671769df4a477ac82e46f0
Anchit Mulye <anchitmulye1005@gmail.com>
2024-04-27 10:28:46 +0530

    Indentation fix

--- Engine/Engine/Log/Logger.cpp
+++ Engine/Engine/Log/Logger.cpp
@@ -7,15 +7,15 @@ namespace Template
     void Logger::Init(const int& logLevel, const char* filePath)
     {
         std::vector<spdlog::sink_ptr> logSinks;
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(filePath, true));
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(filePath, true));
 
-		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-		s_CoreLogger = std::make_shared<spdlog::logger>("Engine", begin(logSinks), end(logSinks));
-		spdlog::register_logger(s_CoreLogger);
-		
+        s_CoreLogger = std::make_shared<spdlog::logger>("Engine", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
+
         spdlog::level::level_enum logLevelInternal;
         switch (logLevel)
         {
@@ -46,6 +46,6 @@ namespace Template
         }
 
         s_CoreLogger->set_level(logLevelInternal);
-		s_CoreLogger->flush_on(logLevelInternal);
+        s_CoreLogger->flush_on(logLevelInternal);
     }
 } // namespace Template
