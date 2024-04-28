681b8370a0e4d80e7a464850f3a4a3868d563313
Anchit Mulye <anchitmulye1005@gmail.com>
2024-04-28 10:56:47 +0530

    Lib and Bin dir for Build

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 PROJECT_HOME=`git rev-parse --show-toplevel`
 BUILD_DIR=Build
+BIN_DIR=Bin
 LOG_DIR=Logs
 
 cd $PROJECT_HOME/$BUILD_DIR
 
 mkdir -p $LOG_DIR
 
-./App/App
+cd $PROJECT_HOME/$BUILD_DIR/$BIN_DIR
+
+./App
